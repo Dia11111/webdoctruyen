e359486feac6bb530122f6ad5850082bcf598b1d
Dia11111 <thocongnghe@gmail.com>
2023-02-13 07:49:47 +0700

    1

---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,37 @@
+<div class="container">
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+
+                      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav mr-auto">
+                          <li class="nav-item active">
+                            <a class="nav-link" href="{{route('home')}}">Admin</a>
+                          </li>
+                          
+                          <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                              Quản lý danh mục
+                            </a>
+                           <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{route('danhmuc.create')}}">Thêm danh mục</a>
+                              <a class="dropdown-item" href="{{route('danhmuc.index')}}">Liệt kê danh mục</a>
+                              
+                            </div>
+                          </li>
+                          <li class="nav-item dropdown">
+                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                              Sách truyện
+                            </a>
+                           <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <a class="dropdown-item" href="{{route('truyen.create')}}">Thêm sách truyện</a>
+                              <a class="dropdown-item" href="{{route('truyen.index')}}">Liệt kê sách truyện</a>
+                              
+                            </div>
+                          </li>
+                        </ul>
+                        <form class="form-inline my-2 my-lg-0">
+                          <input class="form-control mr-sm-2" type="search" placeholder="Tìm kiếm" aria-label="Search">
+                          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Tìm kiếm</button>
+                        </form>
+                      </div>
+                    </nav>
+</div>
